d2beaf59e220883066ca440d03cd0361b1d77673
Carlos Eduardo <67875731+CarlosEddie@users.noreply.github.com>
2020-07-15 17:46:36 -0300

    Update UsaEstoque.java

--- Lista4/Lista 3 com testes/Quest5Quest6eQuest7ComTestes/UsaEstoque.java
+++ Lista4/Lista 3 com testes/Quest5Quest6eQuest7ComTestes/UsaEstoque.java
@@ -3,25 +3,25 @@ public class UsaEstoque {
 	
 	public static void main(String[] args) {
 		Estoque estoque1 = new Estoque("Impressora Jato de Tinta", 13, 6);
-        Estoque estoque2 = new Estoque("Monitor LCD 17 polegadas", 11, 13);
-        Estoque estoque3 = new Estoque("Mouse Ótico", 6, 2);
-        
-        //Dando baixa no estoque1
-        estoque1.darBaixa(5);
-        //Repondo o estoque2
-        estoque2.repor(7);
-        //Dando baixa no estoque3
-        estoque3.darBaixa(4);
-        
-        //Conferindo se é necessario repor algum dos 3 obejetos
-        System.out.println(estoque1.precisaRepor());
-        System.out.println(estoque2.precisaRepor());
-        System.out.println(estoque3.precisaRepor());
-        
-        //Mostranda as informações dos 3 objetos
-        System.out.println(estoque1.mostra());
-        System.out.println(estoque2.mostra());
-        System.out.println(estoque3.mostra());
+		Estoque estoque2 = new Estoque("Monitor LCD 17 polegadas", 11, 13);
+		Estoque estoque3 = new Estoque("Mouse Ã“tico", 6, 2);
+
+		//Dando baixa no estoque1
+		estoque1.darBaixa(5);
+		//Repondo o estoque2
+		estoque2.repor(7);
+		//Dando baixa no estoque3
+		estoque3.darBaixa(4);
+
+		//Conferindo se Ã© necessario repor algum dos 3 obejetos
+		System.out.println(estoque1.precisaRepor());
+		System.out.println(estoque2.precisaRepor());
+		System.out.println(estoque3.precisaRepor());
+
+		//Mostranda as informaÃ§Ãµes dos 3 objetos
+		System.out.println(estoque1.mostra());
+		System.out.println(estoque2.mostra());
+		System.out.println(estoque3.mostra());
 	}
 	
 }
